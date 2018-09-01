ae959e80b31a542bca6bf9c85b4f6dc31f2484cb
Platzi <platzi@gndx.local>
2018-09-01 09:24:43 -0500

    webhook - recibir mensajes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -17,10 +17,21 @@ app.get('/webhook', function(req, response){
     if(req.query['hub.verify_token'] === 'pugpizza_token'){
         response.send(req.query['hub.challenge']);
     } else {
-        response.send('Pug Pizza no tienes permisos.')
+        response.send('Pug Pizza no tienes permisos.');
     }
+});
+
+app.post('/webhook/', function(req, res){
+    const webhook_event = req.body.entry[0];
+    if(webhook_event.messaging) {
+        webhook_event.messaging.forEach(event => {
+            console.log(event);
+        })
+    }
+    res.sendStatus(200);
 })
 
+
 app.listen(app.get('port'), function(){
-    console.log('Nuestro servidor esta funcionando en el puerto', app.get('port'));
-})
+    console.log('Nuestro servidor esta funcionando con el barto en el puerto: ', app.get('port'));
+});
